4a918b9b2f8a68afab5e238e7f7acf999cfcaf81
axelguerrero <guerreroaxel230@yahoo.com>
2022-10-31 16:51:18 -0400

    added styles

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -5,7 +5,13 @@ function GalleryItem(props) {
 
     const simpleView = () => {
         return (
-            <div>
+            <div style={{
+                'width': '25vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin' : '2px',
+                'position': 'relative'
+            }}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -14,7 +20,17 @@ function GalleryItem(props) {
 
     const detailView = () => {
         return (
-            <div>
+            <div style={{
+                'width': '80vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin' : '2px',
+                'position': 'relative',
+                'backgroundImage': `url(${props.item.artworkUrl100})`,
+                'backgroundRepeat': 'no-repeat',
+                'backgroundSize': 'cover',
+                'color': 'white'
+            }}>
                 <h2>{props.item.trackName}</h2>
                 <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
