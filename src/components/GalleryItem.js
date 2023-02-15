a0759bdc0dd7b13b1454cd04208160bdcf782222
axelguerrero <guerreroaxel230@yahoo.com>
2023-02-15 13:17:48 -0500

    added style

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,9 +3,21 @@ import { useState } from "react"
 function GalleryItem(props) {
     let [view, setView] = useState(false)
 
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
+    
+
     const simpleView = () => {
         return (
-            <div>
+            <div style={detailStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
